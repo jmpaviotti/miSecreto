f99a2b2e2e71b58af74ea0fc6375cb702c6ab1bf
unknown <jmpaviotti@gmail.com>
2020-06-29 13:42:54 -0300

    Implement handlebars + db connection

--- app.js
+++ app.js
@@ -4,22 +4,31 @@ const handlebars = require('express-handlebars');
 // Init express
 const app = express();
 
+// Database + test
+const db = require('./db');
+db.query('SELECT NOW() as now', (err, res) => {
+  if (err) {
+    console.log(err.stack);
+  } else {
+    console.log('Connected to database at: ' + res.rows[0]['now']);
+  }
+});
+
 // Handlebars setup
 app.set('view engine', 'hbs');
 app.engine(
   'hbs',
   handlebars({
-    layoutsDir: __dirname + '/views/layouts',
     extname: 'hbs',
   })
 );
 
-// Static public folder for css
+// Static folder for css
 app.use(express.static('public'));
 
 // Create your endpoints/route handlers
 app.get('/', (req, res) => {
-  res.render('main', { layout: 'index' });
+  res.render('index');
 });
 
 // Listen on a port
--- db/index.js
+++ db/index.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 
+// Enviroment variables config
+const dotenv = require('dotenv');
+dotenv.config();
+
 const pool = new Pool();
 
 module.exports = {
