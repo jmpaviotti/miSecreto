a1c27650e2c80621a585d0a35e6db806cdae1fc1
unknown <jmpaviotti@gmail.com>
2020-06-30 02:31:18 -0300

    add insert + functional feed

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const helpers = require('./lib/helpers');
 
 // Init express
 const app = express();
 
 // Database + test
 const db = require('./db');
+const bodyParser = require('body-parser');
 db.query('SELECT NOW() as now', (err, res) => {
   if (err) {
     console.log(err.stack);
@@ -20,16 +22,48 @@ app.engine(
   'hbs',
   handlebars({
     extname: 'hbs',
+    helpers: helpers,
   })
 );
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Static folder for css
 app.use(express.static('public'));
 
-// Create your endpoints/route handlers
+// Endpoints/route handlers
+
 app.get('/', (req, res) => {
-  res.render('index');
+  db.query('SELECT * FROM secretos ORDER BY id DESC LIMIT 10')
+    .then((data) => res.render('index', { rows: data.rows }))
+    .catch((e) => console.error(e.stack));
 });
+
+app.post('/add', (req, res) => {
+  let { author, gender, age, content } = req.body;
+
+  if (gender === 'Masculino') {
+    gender = 'H';
+  } else if (gender === 'Femenino') {
+    gender = 'M';
+  } else {
+    gender = 'N';
+  }
+
+  const text =
+    "INSERT INTO secretos (content, date, time, author, gender, age) VALUES($1, 'now', 'now', $2, $3, $4)";
+  const values = [content, author, gender, age];
+
+  db.query(text, values)
+    .then((r) => {
+      console.log('Success at inserting.');
+      res.redirect('/');
+    })
+    .catch((e) => console.error(e.stack));
+});
+
+// app.use('/secretos', require('./routes/posts'));
 
 // Listen on a port
 const PORT = process.env.PORT || 5000;
