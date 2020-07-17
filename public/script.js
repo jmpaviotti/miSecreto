24b6278e8741dfaf124c63fb6943f3a4e1227d3e
Julian <jmpaviotti@gmail.com>
2020-07-17 05:12:53 -0300

    Fix vote bugs, move to parametrized queries

--- public/script.js
+++ public/script.js
@@ -1,50 +1,52 @@
 window.onload = (event) => {
-  fetch('/secretos/vote')
+  fetch("/secretos/vote")
     .then((response) => response.json())
     .then((data) => {
       for (const [id, value] of Object.entries(data)) {
         const secreto = document.querySelector(`#s${id}`);
-        if (value > 0) {
-          secreto.querySelector('.upvote').classList.add('toggled');
-        } else if (value < 0) {
-          secreto.querySelector('.downvote').classList.add('toggled');
+        if (secreto) {
+          if (value > 0) {
+            secreto.querySelector(".upvote").classList.add("toggled");
+          } else if (value < 0) {
+            secreto.querySelector(".downvote").classList.add("toggled");
+          }
         }
       }
     })
-    .catch((error) => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 };
 
 function vote(id, value) {
   const data = { id: id, vote: value };
 
-  fetch('/secretos/vote', {
-    method: 'POST',
+  fetch("/secretos/vote", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
+      "Content-Type": "application/json; charset=UTF-8",
     },
     body: JSON.stringify(data),
   })
     .then((response) => response.json())
     .then((data) => {
       const secreto = document.querySelector(`#s${id}`);
-      const up = secreto.querySelector('.upvote');
-      const down = secreto.querySelector('.downvote');
-      //
-      secreto.querySelector('.score').innerHTML = data.votes;
-      //
+      const up = secreto.querySelector(".upvote");
+      const down = secreto.querySelector(".downvote");
+
+      secreto.querySelector(".score").innerHTML = data.votes;
+
       if (value == 1) {
-        up.classList.contains('toggled')
-          ? up.classList.remove('toggled')
-          : up.classList.add('toggled');
-        down.classList.remove('toggled');
+        up.classList.contains("toggled")
+          ? up.classList.remove("toggled")
+          : up.classList.add("toggled");
+        down.classList.remove("toggled");
       } else if (value == -1) {
-        down.classList.contains('toggled')
-          ? down.classList.remove('toggled')
-          : down.classList.add('toggled');
-        up.classList.remove('toggled');
+        down.classList.contains("toggled")
+          ? down.classList.remove("toggled")
+          : down.classList.add("toggled");
+        up.classList.remove("toggled");
       }
     })
     .catch((error) => {
-      console.error('Error:', error);
+      console.error("Error:", error);
     });
 }
--- routes/secretos.js
+++ routes/secretos.js
@@ -1,57 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db');
-const h = require('./help');
-const { query } = require('express');
+const db = require("../db");
+const util = require("../lib/util");
+const queries = require("../lib/query");
 
 // Main feed + Pagination
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   if (!req.session.votes) {
     req.session.votes = {};
   }
 
-  h.getPageData(req.query).then((pageData) => {
-    const query = h.sanitizeQuery(req.query, pageData.current)
-
+  util.getPageData(req.query).then((pageData) => {
     if (pageData.current >= 1) {
-      db.query(query)
+      db.query(queries.selectQuery(req.query, pageData.current, pageData.terms))
         .then((data) => {
-          // console.log(data)
-          res.render('index', {
+          res.render("index", {
             rows: data.rows,
             pages: pageData,
             votes: req.session.votes,
-          })
+          });
         })
         .catch((e) => console.error(e.stack));
     } else {
-      res.redirect('/');
+      res.redirect("/");
     }
   });
 });
 
 // Add secreto
-router.post('/add', (req, res) => {
+router.post("/add", (req, res) => {
   let { author, gender, age, content } = req.body;
-  let errors = [];
-
-  if (!author) {
-    errors.push({ text: 'Nombre inválido.' });
-  }
-
-  if (!gender) {
-    errors.push({ text: 'Seleccione género.' });
-  }
-
-  if (age < 18 || age > 100) {
-    errors.push({ text: 'Edad inválida' });
-  }
-
-  if (content.length < 10) {
-    errors.push({ text: 'Secreto muy corto.' });
-  } else if (content.length > 500) {
-    errors.push({ text: 'Secreto muy largo.' });
-  }
+  const errors = util.validateInsert(req.body);
 
   if (errors.length === 0) {
     const text =
@@ -66,53 +45,29 @@ router.post('/add', (req, res) => {
   } else {
     console.log('Caught invalid "Insert" query.');
   }
-  res.redirect('/');
+  res.redirect("/");
 });
 
 // Get session votes
-router.get('/vote', (req, res) => {
+router.get("/vote", (req, res) => {
   res.send(req.session.votes);
 });
 
 // Voting
-router.post('/vote', (req, res) => {
+router.post("/vote", (req, res) => {
   const { id, vote } = req.body;
   const userVotes = req.session.votes;
 
-  if (!userVotes[id]) {
-    db.query(`UPDATE secretos SET votes = votes + ${vote} WHERE id = ${id}`)
-      .then(() => {
-        userVotes[id] = vote; // Track vote
-        db.query(`SELECT votes FROM secretos WHERE id = ${id}`)
-          .then((data) => {
-            res.send(data.rows[0]);
-          })
-          .catch((e) => console.error(e.stack));
-      })
-      .catch((e) => console.error(e.stack));
-  } else if (userVotes[id] == vote) {
-    db.query(`UPDATE secretos SET votes = votes - ${vote} WHERE id = ${id}`) // Undo vote from DB
-      .then(() => {
-        delete userVotes[id]; // Undo vote from session
-        db.query(`SELECT votes FROM secretos WHERE id = ${id}`)
-          .then((data) => {
-            res.send(data.rows[0]);
-          })
-          .catch((e) => console.error(e.stack));
-      })
-      .catch((e) => console.error(e.stack));
-  } else {
-    db.query(`UPDATE secretos SET votes = votes + ${vote * 2} WHERE id = ${id}`) // Undo vote from DB + Count other vote
-      .then(() => {
-        userVotes[id] = vote; // Update session vote
-        db.query(`SELECT votes FROM secretos WHERE id = ${id}`)
-          .then((data) => {
-            res.send(data.rows[0]);
-          })
-          .catch((e) => console.error(e.stack));
-      })
-      .catch((e) => console.error(e.stack));
-  }
+  db.query(queries.voteQuery(id, vote, userVotes))
+    .then(() =>
+      db
+        .query(`SELECT votes FROM secretos WHERE id = $1`, [id])
+        .then((data) => {
+          res.send(data.rows[0]);
+        })
+        .catch((e) => console.error(e.stack))
+    )
+    .catch((e) => console.error(e.stack));
 });
 
 module.exports = router;
